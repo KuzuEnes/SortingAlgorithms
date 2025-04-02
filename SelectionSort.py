08569541a3b78027b63a053ce6d26a2de9f1065f
Enes K. <eneskuzu2003@gmail.com>
2025-04-02 13:49:38 +0300

    .

---
+++ SelectionSort.py
@@ -0,0 +1,13 @@
+def selection_sort(liste):
+    n = len(liste)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if liste[j] < liste[min_index]:
+                min_index = j
+        liste[i], liste[min_index] = liste[min_index], liste[i]
+    return liste
+
+# Kullanım
+sayilar = [64, 25, 12, 22, 11]
+print(selection_sort(sayilar))
